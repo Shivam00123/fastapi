f7e942e7e9951809a73abb68d700c8e42e13b99c
Shivam00123 <shivamjackson6@gmail.com>
2025-09-02 14:08:00 +0530

    modify ecosystem args

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,9 @@
+// ecosystem.config.js
 module.exports = {
   apps: [
     {
       name: "fastserver",
-      script: "./.venv/bin/python3",
+      script: "python3",
       args: "-m uvicorn main:app --host 0.0.0.0 --port 8000",
       env: {
         PYTHONPATH: ".",
